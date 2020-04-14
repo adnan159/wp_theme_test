947c315c9f46f8588f60ea8cde005e94dd5ad90d
adnan159 <osmanhaider159@gmail.com>
2020-04-15 01:48:48 +0600

    contact form page re-designed

--- contact.php
+++ contact.php
@@ -12,21 +12,7 @@ Template Name: Contact;
 			<div class="col-2-3">
 				<div class="wrap-col">
 					<?php while(have_posts()) : the_post(); ?>
-
-						<article>
-							<?php the_post_thumbnail(); ?>
-							<h2><a href="<?php the_permalink(); ?>"><?php the_title();?></a></h2>
-							<div class="info">[By <?php the_author(); ?> on <?php the_time('F d, Y');?> with <?php comments_popup_link('No comment Here','One commnet','%d comments','adnan','Commnet Disable');?>]</div>
-							
 							<?php the_content();?>
-
-							<div class="comment">
-								
-								<?php comments_template(); ?>
-
-							</div>
-						</article>
-
 					<?php endwhile; ?>
 				</div>
 			</div>
